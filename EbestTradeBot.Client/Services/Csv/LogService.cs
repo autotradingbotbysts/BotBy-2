6e005a001f742c610a903092cddc93a7e5af2235
tksgo1995 <36852046+tksgo1995@users.noreply.github.com>
2024-12-27 18:28:22 +0900

    log에 lock 추가

--- EbestTradeBot.Client/Services/Csv/LogService.cs
+++ EbestTradeBot.Client/Services/Csv/LogService.cs
@@ -1,7 +1,6 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
 using EbestTradeBot.Shared.Models.Log;
-using System;
 using System.Globalization;
 using System.IO;
 using System.Threading.Tasks;
@@ -11,22 +10,26 @@ namespace EbestTradeBot.Client.Services.Log
     public class LogService : ILogService
     {
         private readonly string _filePath = @".\";
+        private static object _lock = new();
 
         public async Task WriteLog(LogModel model)
         {
-            bool fileExists = File.Exists(_filePath + "log.csv");
-
-            using var writer = new StreamWriter(_filePath + "log.csv", true);
-            using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = !fileExists });
-            
-            if(!fileExists)
+            lock (_lock)
             {
-                csv.WriteHeader<LogModel>();
+                bool fileExists = File.Exists(_filePath + "log.csv");
+
+                using var writer = new StreamWriter(_filePath + "log.csv", true);
+                using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = !fileExists });
+
+                if (!fileExists)
+                {
+                    csv.WriteHeader<LogModel>();
+                    csv.NextRecord();
+                }
+
+                csv.WriteRecord(model);
                 csv.NextRecord();
             }
-
-            csv.WriteRecord(model);
-            csv.NextRecord();
         }
     }
 }
